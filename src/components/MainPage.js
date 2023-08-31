f6224192282cde38b215ec8a97f5a7ab627e5141
Gisele Fox <72312641+giselejfox@users.noreply.github.com>
2023-08-30 23:44:42 -0700

    navbar setup and sketch out homepage

---
+++ src/components/MainPage.js
@@ -0,0 +1,21 @@
+
+export default function MainPage() {
+    return (
+        <div>
+            {/* <Hero /> */}
+            {/* <Intro /> */}
+            {/* <Divider /> */}
+            {/* <Gallery /> */}
+            {/* <StayUpdated /> */}
+        </div>
+    )
+}
+
+
+function Hero() {
+    return (
+        <div className="vh-100">
+
+        </div>
+    )
+}
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,9 +2,14 @@
 
 export default function Navbar() {
     return(
-        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <nav className="navbar navbar-expand-lg my-5 py-5 mx-5">
             <div className="container-fluid">
-                <a className="navbar-brand" href="#">Shoebox Wonders</a>
+                <a className="navbar-brand d-flex align-content-center" href="#">
+                    <svg className="align-self-center me-3" width="24" height="24" viewBox="0 0 276 266" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M102.811 26.3008C113.887 -7.78768 162.113 -7.78765 173.189 26.3008L180.209 47.9048C185.162 63.1497 199.368 73.4712 215.398 73.4712H238.113C273.956 73.4712 288.859 119.337 259.861 140.405L241.484 153.757C228.516 163.179 223.09 179.879 228.043 195.124L235.063 216.728C246.139 250.816 207.123 279.163 178.125 258.095L159.748 244.743C146.78 235.321 129.22 235.321 116.252 244.743L97.8745 258.095C68.8771 279.163 29.8614 250.816 40.9374 216.728L47.9569 195.124C52.9103 179.879 47.4839 163.179 34.5159 153.757L16.1385 140.405C-12.8589 119.337 2.04378 73.4712 37.8865 73.4712H60.6023C76.6317 73.4712 90.838 63.1496 95.7914 47.9048L102.811 26.3008Z" fill="black"/>
+                    </svg>
+                    Shoebox Wonders
+                </a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
                 </button>
@@ -14,10 +19,10 @@ export default function Navbar() {
                     <a className="nav-link active" aria-current="page" href="#">Exhibits</a>
                     </li>
                     <li className="nav-item">
-                    <a className="nav-link" href="#">Events</a>
+                    <a className="nav-link active" href="#">Events</a>
                     </li>
                     <li className="nav-item">
-                    <a className="nav-link" href="#">Contact</a>
+                    <a className="nav-link active" href="#">Contact</a>
                     </li>
                 </ul>
                 </div>
